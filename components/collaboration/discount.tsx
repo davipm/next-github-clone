b1e1b6168e3ed8c2646b00735e257526e293aaf6
Davi Pereira <davi.p.m94@gmail.com>
2023-12-08 00:07:06 -0300

    update apps

--- components/collaboration/discount.tsx
+++ components/collaboration/discount.tsx
@@ -9,7 +9,25 @@ export default function Discount() {
   return (
     <div className="flex relative md:pl-10">
       <img src="https://github.githubassets.com/images/modules/site/home-campaign/bg-stars-1.webp" alt="" aria-hidden="true" width="2712" height="2712" />
-      <div></div>
+      <div className="flex items-center flex-row relative">
+        <motion.div initial={{ height: 0 }} whileInView={{ height: '100%' }} className="w-[3px] h-full increase1"></motion.div>
+        <img
+          src="https://github.githubassets.com/images/modules/site/home-campaign/git-branch-collaboration.svg"
+          alt=""
+          aria-hidden="true"
+          width="81"
+          height="485"
+          style={{
+            left: 'calc(50% - 2px)',
+            height: 'auto',
+            maxWidth: '9vw',
+            transitionDelay: '100ms'
+          }}
+          className="hidden md:block absolute bottom-4 left-0"
+          loading="lazy"
+          decoding="async"
+        />
+      </div>
     </div>
   )
 }
