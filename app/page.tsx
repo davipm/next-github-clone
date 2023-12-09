c62ddef8eb2fb803741648f73f6b2c8ff9168180
Davi Pereira <davi.p.m94@gmail.com>
2023-12-08 21:52:27 -0300

    update hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "@/components/navbar";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
@@ -7,6 +8,20 @@ export default function Home() {
       <div className="relative z-50">
         <div className="absolute w-full">
           <Navbar />
+        </div>
+      </div>
+
+      <div className="overflow-hidden">
+        <div className="relative">
+          <img
+            src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp"
+            alt="Bg image"
+            className="absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] -z-30 image"
+          />
+        </div>
+
+        <div className="px-3">
+          <Hero />
         </div>
       </div>
     </main>
