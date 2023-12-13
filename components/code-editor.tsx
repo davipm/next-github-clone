316bc1aa601802e7fdc5c0461d0894d85a79ad7d
Davi Pereira <davi.p.m94@gmail.com>
2023-12-13 13:41:44 -0300

    finishing code editor

--- components/code-editor.tsx
+++ components/code-editor.tsx
@@ -1,25 +1,13 @@
 "use client";
 
-import { motion } from "framer-motion";
-import { GoCode, GoPlus } from "react-icons/go";
+import { GoCode, GoPlus, GoBell } from "react-icons/go";
 import { FiMenu, FiLock } from "react-icons/fi";
 import { TfiReload } from "react-icons/tfi";
+import { FaGithub, FaRegBell } from "react-icons/fa";
 
-const container = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 0,
-    transition: {
-      staggerChildren: 0.1,
-      delayChildren: 0.1,
-    },
-  },
-};
-
-const item = {
-  hidden: { opacity: 0, y: 3 },
-  show: { opacity: 1, y: 0, transition: { type: "keyframe" } },
-};
+import CodeBlock from "@/components/code-block";
+import ConsoleAnimated from "@/components/console-animated";
+import Image from "next/image";
 
 export default function CodeEditor() {
   return (
@@ -43,6 +31,53 @@ export default function CodeEditor() {
       <div className="flex items-stretch">
         <div className="hidden md:block p-6 border-r border-[#30363d]">
           <FiMenu size={30} className="text-[#7d8590]" />
+        </div>
+        <div className="min-w-0 w-full">
+          <div className="border-b border-[#30363d] text-[#7d8590]">
+            <ul className="flex px-3 pt-4">
+              <li className="px-3 py-2 rounded-t-md bg-[#0d1117] text-gray-300">index.html</li>
+              <li className="px-3 py-2">script.js</li>
+              <li className="px-3 py-2">package.json</li>
+            </ul>
+
+            <div className="bg-[#0d1117] p-4">
+              <div className="flex">
+                <div>
+                  {new Array(12).fill("").map((_, index) => (
+                    <div className="pr-2" key={index + 1}>
+                      {index + 1}
+                    </div>
+                  ))}
+                </div>
+                <CodeBlock />
+              </div>
+            </div>
+          </div>
+          <div className="p-4 text-[#7d8590]">
+            <ul className="flex mb-3">
+              <li className="pr-3 text-gray-200">Terminal</li>
+              <li className="pr-3">Output</li>
+              <li className="pr-3">Problems</li>
+              <li className="pr-3">Debug Console</li>
+            </ul>
+
+            <ConsoleAnimated />
+          </div>
+        </div>
+        <div className="md:block hidden border-l border-[#30363d] w-full">
+          <div className="flex items-center justify-between px-[16px] pt-[22px] pb-2 text-gray-100">
+            <FiMenu size={25} />
+            <FaGithub size={25} />
+            <FaRegBell sieze={25} />
+          </div>
+          <div className="px-6 flex items-center justify-between h-full bg-[#040d21]">
+            <Image
+              src="/globe.jpg"
+              alt="Planet earth with visualization of GitHub activity crossing the globe"
+              width={1238}
+              height={1404}
+            />
+          </div>
         </div>
       </div>
     </div>
---
+++ components/console-animated.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.1,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: 3 },
+  show: { opacity: 1, y: 0, transition: { type: "keyframe" } },
+};
+
+export default function ConsoleAnimated() {
+  return (
+    <motion.div
+      variants={container}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.5 }}
+      className="overflow-hidden whitespace-nowrap font-mono text-[14px] text-[#7d8590]"
+      data-type-row-delay="50"
+    >
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:36</span>]<span> Starting </span>
+        &apos;<span className="color-fg-accent">watch-extension:vscode-api-tests</span>&apos; ...
+      </motion.div>
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:36</span>]<span> Finished </span>
+        &apos;<span className="color-fg-accent">clean-extension:typescript-language-features</span>&apos; after
+        <span className="color-fg-done">248 ms</span>
+      </motion.div>
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:36</span>]<span> Starting </span>
+        &apos;<span className="color-fg-accent">watch-extension:typescript-language-features</span>&apos; ...
+      </motion.div>
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:36</span>]<span> Finished </span>
+        &apos;<span className="color-fg-accent">clean-extension:php-language-features</span>&apos; after
+        <span className="color-fg-done">384 ms</span>
+      </motion.div>
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:36</span>]<span> Starting </span>
+        &apos;<span className="color-fg-accent">watch-extension:php-language-features</span>&apos; ...
+      </motion.div>
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:40</span>]<span> Finished </span>
+        &apos;<span className="color-fg-accent">clean-extension:html-language-features-server</span>&apos; after
+        <span className="color-fg-done">4.66 s</span>
+      </motion.div>
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:40</span>]<span> Starting </span>
+        &apos;<span className="color-fg-accent">watch-extension:html-language-features-server</span>&apos; ...
+      </motion.div>
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:43</span>]<span> Finished </span>
+        &apos;<span className="color-fg-accent">clean-client</span>&apos; after
+        <span className="color-fg-done">7.33 s</span>
+      </motion.div>
+      <motion.div variants={item} className="no-wrap js-type-row row-is-visible">
+        [<span className="color-fg-subtle">09:43:43</span>]<span> Starting </span>
+        &apos;<span className="color-fg-accent">watch-client</span>&apos; ...
+      </motion.div>
+    </motion.div>
+  );
+}
