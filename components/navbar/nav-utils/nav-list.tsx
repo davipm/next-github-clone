56250d091bb6583c40ebb26e4a8b7ecba0bfbf3d
Davi Pereira <davi.p.m94@gmail.com>
2023-12-12 22:37:40 -0300

    update icons

--- components/navbar/nav-utils/nav-list.tsx
+++ components/navbar/nav-utils/nav-list.tsx
@@ -1,28 +1,17 @@
 "use client";
 
+import { ReactNode } from "react";
+
 interface Props {
   main: string;
   submain: string;
-  path: string;
-  path2: string;
+  icon: ReactNode;
 }
 
-export default function NavList({ path, path2, submain, main }: Props) {
+export default function NavList({ icon, submain, main }: Props) {
   return (
     <a href="#" className="group py-3 flex items-center space-x-3 cursor-pointer">
-      <svg
-        aria-hidden="true"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        version="1.1"
-        width="24"
-        data-view-component="true"
-        className="mr-3 transition ease-in duration-100 text-neutral-800 group-hover:text-blue-600"
-      >
-        <path d={path}></path>
-        <path d={path2}></path>
-      </svg>
+      <span className="text-neutral-800 group-hover:text-blue-600">{icon}</span>
 
       <div className="transition ease-in duration-100 text-sm text-neutral-500 group-hover:text-blue-600">
         <div className="font-semibold transition ease-in duration-100 text-base leading-5 text-neutral-800 group-hover:text-blue-600">
